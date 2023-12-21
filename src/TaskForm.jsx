808ea827a86199db1b8540bb0a7b5dca82994f65
fuatX <fuatyagizaydin@hotmail.com>
2023-12-21 17:29:36 +0300

    Adding the Information as a New Task

--- src/TaskForm.jsx
+++ src/TaskForm.jsx
@@ -1,80 +1,72 @@
+import { useState } from "react";
+
 export default function TaskForm() {
+  const emptyForm = {
+    task: "",
+    priority: false,
+  };
+  const [formData, setFormData] = useState(emptyForm);
+  const [tasks, setTasks] = useState([]);
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+
+    console.log(formData);
+    if (formData.task.length > 3) {
+      setTasks((prev) => [formData, ...prev]); // form data en sona yada en başa
+      setFormData(emptyForm);
+      event.target.reset();
+      // console.log(tasks);
+    }
+  }
+
+  function handleInputChange(event) {
+    setFormData((prev) => {
+      return {
+        ...prev,
+        [event.target.name]:
+          event.target.type === "checkbox"
+            ? event.target.checked
+            : event.target.value,
+      };
+    });
+  }
+
   return (
     <>
-      <form>
+      <ul>
+        {tasks.map((item, index) => (
+          <li key={index}>{item.task}</li>
+        ))}
+      </ul>
+      <form onSubmit={handleFormSubmit}>
         <div className="row mb-3">
-          <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">
-            Email
-          </label>
-          <div className="col-sm-10">
-            <input type="email" className="form-control" id="inputEmail3" />
-          </div>
-        </div>
-        <div className="row mb-3">
-          <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">
-            Password
+          <label htmlFor="task" className="col-sm-2 col-form-label">
+            Task
           </label>
           <div className="col-sm-10">
             <input
-              type="password"
+              type="text"
               className="form-control"
-              id="inputPassword3"
+              id="task"
+              name="task"
+              onChange={handleInputChange}
             />
           </div>
         </div>
-        <fieldset className="row mb-3">
-          <legend className="col-form-label col-sm-2 pt-0">Radios</legend>
-          <div className="col-sm-10">
-            <div className="form-check">
-              <input
-                className="form-check-input"
-                type="radio"
-                name="gridRadios"
-                id="gridRadios1"
-                value="option1"
-                defaultChecked
-              />
-              <label className="form-check-label" htmlFor="gridRadios1">
-                First radio
-              </label>
-            </div>
-            <div className="form-check">
-              <input
-                className="form-check-input"
-                type="radio"
-                name="gridRadios"
-                id="gridRadios2"
-                value="option2"
-              />
-              <label className="form-check-label" htmlFor="gridRadios2">
-                Second radio
-              </label>
-            </div>
-            <div className="form-check disabled">
-              <input
-                className="form-check-input"
-                type="radio"
-                name="gridRadios"
-                id="gridRadios3"
-                value="option3"
-                disabled
-              />
-              <label className="form-check-label" htmlFor="gridRadios3">
-                Third disabled radio
-              </label>
-            </div>
-          </div>
-        </fieldset>
+
         <div className="row mb-3">
           <div className="col-sm-10 offset-sm-2">
             <div className="form-check">
               <input
                 className="form-check-input"
                 type="checkbox"
-                id="gridCheck1"
+                id="priority"
+                name="priority"
+                onChange={handleInputChange}
               />
-              <label className="form-check-label" htmlFor="gridCheck1">
-                Example checkbox
+              <label className="form-check-label" htmlFor="priority">
+                Oncelikli
               </label>
             </div>
           </div>
@@ -82,7 +74,7 @@ export default function TaskForm() {
         <div className="row mb-3">
           <div className="col-sm-10 offset-sm-2">
             <button type="submit" className="btn btn-primary">
-              Sign in
+              Kaydet
             </button>
           </div>
         </div>
